1a603791394c66e283cdfd1521260440e2ec4407
cristian3629 <cristian.gonzalez@despegar.com>
2017-10-11 01:10:13 -0300

    actualizo interfez REST user en documentacion se agrega el router con los distintos verbos REST para el servicio de user

--- src/main/user/user_controller.py
+++ src/main/user/user_controller.py
@@ -2,7 +2,20 @@ from flask import Blueprint
 
 user_controller = Blueprint('controller',__name__)
 
+@user_controller.route('/user',methods = ['POST'])
+def add_user():
+    return 'Add new user'
 
-@user_controller.route('/user')
-def root_service():
-    return 'Welcome to user service'
+
+@user_controller.route('/user',methods = ['PUT'])
+def update_user():
+    return 'Update a user'
+
+
+@user_controller.user('/user/<id>',methods = ['GET'])
+def see_user(id):
+    return 'See user information'id
+
+@user_controller.user('/user/<id>',methos = ['DELETE'])
+def delete_user(id):
+    return 'Delete user'+id
