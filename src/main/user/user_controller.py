5c678676e9d36494695021308d1daca08f17d1ba
Tomás Arjovsky <t.arjovsky@gmail.com>
2017-10-12 17:21:03 -0300

    añado últimos cambios intentando conectar desde python

--- src/main/user/user_controller.py
+++ src/main/user/user_controller.py
@@ -1,8 +1,15 @@
 from flask import Blueprint
+from pymongo import MongoClient
+
 
 user_controller = Blueprint('controller',__name__)
 
 
 @user_controller.route('/user')
 def root_service():
+    # ingreso a alguien.
+    client = MongoClient()
+    db = client.t2
+    result = db.users.insert_one({"name": "tomas", "age": 24})
+
     return 'Welcome to user service'
