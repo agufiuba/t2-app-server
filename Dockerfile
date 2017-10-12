baba2a75ce9f7568301bf4c9892601a99874f8bc
Tomás Arjovsky <t.arjovsky@gmail.com>
2017-10-12 17:44:14 -0300

    conexión por nombre de servicio docker.

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,6 @@ ADD src /as/src/main
 ADD src /as/src/test
 ADD requirements.txt /as/requirements.txt
 
-# RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-
-# RUN apt-get update && apt-get install postgresql-client -y
 RUN pip install --index-url=http://pypi.python.org/simple/ --trusted-host pypi.python.org -r requirements.txt
 
 EXPOSE 3000
--- src/main/user/user_controller.py
+++ src/main/user/user_controller.py
@@ -8,8 +8,8 @@ user_controller = Blueprint('controller',__name__)
 @user_controller.route('/user')
 def root_service():
     # ingreso a alguien.
-    client = MongoClient()
+    client = MongoClient("mongodb://mdb") # Esto anda en el docker. Y en heroku?
     db = client.t2
-    result = db.users.insert_one({"name": "tomas", "age": 24})
+    result = db.users.find()
 
-    return 'Welcome to user service'
+    return str([str(doc) for doc in db.users.find()])
