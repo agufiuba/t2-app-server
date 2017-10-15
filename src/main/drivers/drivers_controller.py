3860751c380181fefb20861a01f07508ca8dd228
Tomás Arjovsky <t.arjovsky@gmail.com>
2017-10-15 20:25:16 -0300

    Se añade get al servicio de choferes disponibles y se quita el de usuarios

--- src/main/drivers/drivers_controller.py
+++ src/main/drivers/drivers_controller.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
+from pymongo import MongoClient
+from bson.json_util import dumps
 
 drivers_controller = Blueprint('drivers_controller',__name__)
 
-
 @drivers_controller.route('/drivers')
 def root_service():
-    return 'Welcome to driver service'
+    # Esto anda en el docker. Y en heroku?
+    client = MongoClient("mongodb://mdb")
+    db = client.t2
+    # Conversión de BSON a JSON.
+    return dumps([doc for doc in db.drivers.find()])
--- src/main/user/user_controller.py
+++ src/main/user/user_controller.py
@@ -1,15 +1,6 @@
 from flask import Blueprint
-from pymongo import MongoClient
-
-
 user_controller = Blueprint('controller',__name__)
-
 
 @user_controller.route('/user')
 def root_service():
-    # ingreso a alguien.
-    client = MongoClient("mongodb://mdb") # Esto anda en el docker. Y en heroku?
-    db = client.t2
-    result = db.users.find()
-
-    return "{" + str([str(doc) for doc in db.users.find()]) + "}"
+    return 'Welcome to user service' #NOTE vuelve a como estaba antes.
