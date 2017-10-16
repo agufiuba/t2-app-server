d3ed765962e22894fec69f93acc2ff2bc6849143
cristian3629 <cristian.gonzalez@despegar.com>
2017-10-16 11:45:04 -0300

    agrego para que devuelva un 200

--- src/main/login/login_controller.py
+++ src/main/login/login_controller.py
@@ -6,4 +6,4 @@ login_controller = Blueprint('login_controller',__name__)
 @login_controller.route('/login',methods=['POST'])
 def root_service():
     login_service.login(request)
-    return 'Welcome to login service'
+    return 'Welcome to login service',200
--- src/main/login/login_service.py
+++ src/main/login/login_service.py
@@ -3,3 +3,4 @@ from ..main.firebase import firebase_service
 def login(request):
     id_token = request.form['Authorization']
     firebase_service.validate_token(id_token)
+    return 'Login correct',200
