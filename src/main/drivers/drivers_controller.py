ca35564c75c4f3d5db456dbde20d66dd227b98f4
Tomás Arjovsky <t.arjovsky@gmail.com>
2017-10-15 22:09:18 -0300

    añado PUT del servicio de drivers

--- src/main/drivers/drivers_controller.py
+++ src/main/drivers/drivers_controller.py
@@ -4,10 +4,21 @@ from bson.json_util import dumps
 
 drivers_controller = Blueprint('drivers_controller',__name__)
 
-@drivers_controller.route('/drivers')
-def root_service():
+@drivers_controller.route('/drivers', methods=["GET"])
+def get_drivers():
     # Esto anda en el docker. Y en heroku?
     client = MongoClient("mongodb://mdb")
     db = client.t2
     # Conversión de BSON a JSON.
-    return dumps([doc for doc in db.drivers.find()])
+    return dumps([doc for doc in db.drivers.find()]), 200
+
+@drivers_controller.route('/drivers/<id>', methods=["PUT"])
+def login_driver(id):
+    client = MongoClient("mongodb://mdb")
+    db = client.t2
+    # TODO chequear que existe ese id en el shared server?
+    # TODO chequear que no está dado de alta.
+    # TODO chequear que id sea un número? Son numéricos
+    # Por ahora lo doy de alta así nomás.
+    db.drivers.insert_one({ "id": int(id) })
+    return 'PUT driver OK', 200
--- src/main/user/user_controller.py
+++ src/main/user/user_controller.py
@@ -3,4 +3,4 @@ user_controller = Blueprint('controller',__name__)
 
 @user_controller.route('/user')
 def root_service():
-    return 'Welcome to user service' #NOTE vuelve a como estaba antes.
+    return 'Welcome to user service'
