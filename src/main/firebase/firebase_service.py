ed0b427387d92ccc212d4a4b8089c5d1deea0dab
cristian3629 <cristian.gonzalez@despegar.com>
2017-10-16 11:39:54 -0300

    agrego para que se imprimia el decoded_token

--- src/main/firebase/firebase_service.py
+++ src/main/firebase/firebase_service.py
@@ -10,5 +10,6 @@ auth = firebase_admin.initialize_app(cred)
 def validate_token(id_token):
     decoded_token = auth.verify_id_token(id_token)
     uid = decoded_token['uid']
+    print(decoded_token)
     print(uid)
     return True
