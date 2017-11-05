61e59a5ace558d0f8e6468016bdeb7df74b8cdd0
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-05 02:53:30 -0300

    tratando de arregla fix con auth de firebase (2)

--- src/main/firebase/firebase_service.py
+++ src/main/firebase/firebase_service.py
@@ -1,18 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials
-
+from firebase_admin import auth
 import logging
 FORMAT = "%(asctime)-15s    %(service)-8s     %(message)s"
 logging.basicConfig(format=FORMAT,level=logging.INFO)
 log_info = {'clientip': '192.168.0.1', 'service': 'firebaseService'}
 
+logging.info('Obteniendo credenciales',extra=log_info)
+cred = credentials.Certificate('/as/src/main/firebase/serviceAccountKey.json')
+logging.info('Levantanto la app de firebase',extra=log_info)
+default_app = firebase_admin.initialize_app(cred)
 
 
 def validate_token(id_token):
-    logging.info('Obteniendo credenciales',extra=log_info)
-    cred = credentials.Certificate('/as/src/main/firebase/serviceAccountKey.json')
-    logging.info('Levantanto la app de firebase',extra=log_info)
-    auth = firebase_admin.initialize_app(cred)
 
 
     decoded_token = auth.verify_id_token(id_token)
