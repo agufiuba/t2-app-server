c9a3d5658e63d0f77af4f521c29447022a0cdfd0
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-05 19:47:13 -0300

    se devuelvo un json object con el mensaje

--- src/llevame/login/login_controller.py
+++ src/llevame/login/login_controller.py
@@ -20,6 +20,6 @@ def login_user():
     logging.info('Se recibio un POST en /login',extra=log_info)
     if loginService.login(request):
         logging.info('El logue fue exitoso, devolviendo un 200',extra=log_info)
-        return 'El logueo fue exitoso',200
+        return jsonify({'message':'El logueo fue exitoso'}),200
     logging.info('El logue falló, devolviendo un 400',extra=log_info)
-    return 'El logueo no se pudo realizar, verificio que el usuario esté registrado',400
+    return jsonify({'message':'El logueo no se pudo realizar, verificio que el usuario esté registrado'}),400
