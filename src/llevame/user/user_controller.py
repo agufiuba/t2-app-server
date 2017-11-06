7d976487f8e4abd3fd8e1ca2037d07f2a8d27143
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-06 14:20:10 -0300

    se agregar para que el user controller devuelva un json y no string directamente

--- src/llevame/user/user_controller.py
+++ src/llevame/user/user_controller.py
@@ -18,10 +18,10 @@ def add_user():
     logging.info('Se recibio un Request POST',extra=log_info)
     response = user_validator.validateAddUserRequest(request)
     if response != 'ok':
-        return response,400
+        return jsonify({'message':response}),400
     if userService.addUser(request.get_json()):
-        return 'POST user OK',200
-    return 'POST user is not OK',400
+        return jsonify({'message':'El registro del usuario fue exitoso'}),200
+    return jsonify({'message':'hubo un error al tratar de agregar el usuario'}),400
 
 
 @user_controller.route('/user', methods=['PUT'])
@@ -30,7 +30,7 @@ def update_user():
     if response != 'ok':
         return response,400
     logging.info('Se recibio un Request PUT', extra=log_info)
-    return 'PUT user OK', 200
+    return jsonify({'message':'Se actualizo la información del usuario de manera correcta'}), 200
 
 
 @user_controller.route('/user/<email>',methods=['GET'])
@@ -39,9 +39,9 @@ def see_user(email):
     user = userService.getUser(email)
     if (user != None):
         return jsonify(user),200
-    return 'Error',400
+    return jsonify({'message':'Hubo un error al tratar de obtener la información de usuario'+email}),400
 
 @user_controller.route('/user/<id>', methods=['DELETE'])
 def delete_user(id):
     logging.info('Se recibio un Request DELETE', extra=log_info)
-    return 'DELETE user OK', 200
+    return jsonify({'message':'Se pudo eliminar el usuario de manera correcta'}), 200
