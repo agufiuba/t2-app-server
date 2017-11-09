42c35a5928e176cad315b4aaea6278735186dbe7
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-09 01:10:01 -0300

    el servicio de position ya esta escrito, guarda position, ademas verifica si el usuario esta viajando

--- src/llevame/position/position_controller.py
+++ src/llevame/position/position_controller.py
@@ -1,8 +1,17 @@
 from flask import Blueprint
+from firebase import firebase_service as firebaseService
+from . import positionRequestValidator
+import request
 
 position_controller = Blueprint('position_controller',__name__)
 
 
-@position_controller.route('/position/<id>')
-def root_service():
+positions_from_user = {}
+
+@position_controller.route('/position')
+def addPositionInTime():
+    token = request.headers['Authorization']
+    email = firebaseService.validate_token(token)
+    validation = positionRequestValidator.validate(request)
+
     return 'Welcome to positioning service'
