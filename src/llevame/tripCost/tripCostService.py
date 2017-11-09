ea3ee3aa7a2990d504b5cb563b15f716fc92dd09
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-09 00:12:19 -0300

    se pasa el text en vez del value del response de google y emula el cost

--- src/llevame/tripCost/tripCostService.py
+++ src/llevame/tripCost/tripCostService.py
@@ -12,14 +12,14 @@ url = "https://maps.googleapis.com/maps/api/directions/json?"
 def getCostAndDistance(userEmail,fromString,toString):
     dictWithSomeParameters = getDistanceInKm(fromString,toString)
     cost = sharedService.getCostFromDistanceInKM(userEmail,dictWithSomeParameters['distance'])
-    dictWithSomeParameters['cost'] = cost
+    dictWithSomeParameters['cost'] = 0
     return dictWithSomeParameters
 
 
 def getDistanceInKm(fromString,toString):
     res = requests.get(url+'origin='+fromString+'&destination='+toString+'&key='+apiKey+"&alternatives=true")
     resJSON = json.loads(res.text)
-    distanceInMeters = resJSON['routes'][0]['legs'][0]['distance']['value']
-    timeInMinutes = resJSON['routes'][0]['legs'][0]['duration']['text'].split(' ')[0]
+    distanceInMeters = resJSON['routes'][0]['legs'][0]['distance']['text']
+    timeInMinutes = resJSON['routes'][0]['legs'][0]['duration']['text']
     points = resJSON['routes'][0]['overview_polyline']['points']
-    return {'distance':distanceInMeters/1000,'time':timeInMinutes,'points':points}
+    return {'distance':distanceInMeters,'time':timeInMinutes,'points':points}
