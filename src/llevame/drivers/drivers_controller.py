966f7a55aed77f89eab16874c34ac7a8ee1d7c62
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-27 22:31:09 -0300

    se agrega para que ahora se obtenga de firebase la posicion de cada chofer disponible

--- src/llevame/drivers/drivers_controller.py
+++ src/llevame/drivers/drivers_controller.py
@@ -1,22 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint,request,jsonify
+from firebase import firebase_service as firebaseService
+from . import driver_service as driverService
 
-def drivers_controller_constructor(driver_service):
+drivers_controller = Blueprint('drivers_controller',__name__)
 
-    drivers_controller = Blueprint('drivers_controller',__name__)
 
-    @drivers_controller.route('/drivers', methods=["GET"])
-    def get_drivers():
-        # Conversión de BSON a JSON.
-        return driver_service.get_drivers(), 200
 
-    @drivers_controller.route('/drivers/<id>', methods=["PUT"])
-    def login_driver(id):
-        # client = MongoClient("mongodb://mdb")
-        # TODO chequear que existe ese id en el shared server?
-        # TODO chequear que no está dado de alta.
-        # TODO chequear que id sea un número? Son numéricos
-        # Por ahora lo doy de alta así nomás.
-        driver_service.login_driver(id)
-        return 'PUT driver OK', 200
 
-    return drivers_controller
+#Obtengo todos los choferes que esten cerca del pasajero que quiere viajar
+@drivers_controller.route('/drivers', methods=["GET"])
+def get_drivers():
+    token = request.headers['Authorization']
+    email = firebaseService.validate_token(token)
+    body = request.get_json()
+    drivers = driverService.getDriversFrom(body['from'])
+    return jsonify({'drivers':drivers}),200
+
+
+
+@drivers_controller.route('/drivers/', methods=["POST"])
+def login_driver():
+    token = request.headers['Authorization']
+    email = firebaseService.validate_token(token)
+    ID = firebaseService.getUID(token)
+    driverSrvice.login_driver(ID)
+    return jsonify({'message':'Se agrego de manera correcta al chofer de email'+email}),200
--- src/llevame/drivers/drivers_service.py
+++ src/llevame/drivers/drivers_service.py
@@ -1,18 +1,17 @@
 from pymongo import MongoClient
 from bson.json_util import dumps
+import sys
 
-class DriversService():
-    def __init__(self, db_url, db = "t2"):
-        self.client = MongoClient(db_url)
-        self.db = self.client[db] # TODO añadir opción de cambio de BDD.
+this = sys.modules[__name__]
+this.drivers = []
 
-    # Devuelve un BSON con los choferes disponibles.
-    def get_drivers(self):
-        return dumps([doc for doc in self.db.drivers.find()])
 
-    # Añade a la BDD un chofer.
-    def login_driver(self, id):
-        self.db.drivers.insert_one({ "id": int(id) })
 
-    # TODO Lo quita de la base de datos.
-    # def logout_driver(self, id):
+# Devuelve un BSON con los choferes disponibles.
+def get_drivers():
+    return true
+
+# El ID es firebase id
+def login_driver(ID):
+    this.drivers.push(ID)
+    return true;
