7ac4b96387522685d3c21a768a145ee99a7158bc
Tomás Arjovsky <t.arjovsky@gmail.com>
2017-12-01 01:10:41 -0300

    remove path (deprecated)

--- src/llevame/app.py
+++ src/llevame/app.py
@@ -3,7 +3,6 @@ from user.user_controller import build_user_controller
 from availableTrips.availableTripsController import build_available_trips_controller
 from position.position_controller import position_controller
 from drivers.drivers_controller import build_drivers_controller
-from path.path_controller import path_controller
 from parameters.parametersController import parameters_controller
 from login.login_controller import build_login_controller
 from shared_service import shared_server_service as SharedService
@@ -15,8 +14,7 @@ from my_firebase import firebase_service
 import os
 import logging
 
-
-#Configuración del loggin
+# Configuración del loggin
 FORMAT = "%(asctime)-15s    %(service)-8s     %(message)s"
 logging.basicConfig(format=FORMAT,level=logging.INFO)
 log_info = {'clientip': '192.168.0.1', 'service': 'user'}
@@ -42,7 +40,6 @@ def build_app(interfaces = None):
     interfaces.get_shared_server_service().getToken()
 
     app.register_blueprint(build_user_controller(interfaces))
-    app.register_blueprint(path_controller)
     app.register_blueprint(build_available_trips_controller(interfaces))
     app.register_blueprint(build_drivers_controller(interfaces))
     app.register_blueprint(position_controller)
