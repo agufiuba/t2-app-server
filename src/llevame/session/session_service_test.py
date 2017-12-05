b3f7a31163bcfaafc825bf43cfb16410b713a388
cristian3629 <cristian3629@gmail.com>
2017-12-04 23:39:55 -0300

    agrego los test para el servicio de session

---
+++ src/llevame/session/session_service_test.py
@@ -0,0 +1,16 @@
+import unittest
+from session import session_service as sessionService
+
+
+
+class TestSessionService(unittest.TestCase):
+    def test_the_session_list_should_be_empty_when_the_userID_is_not(self):
+        assertTrue(sessionService.getSessionsList('userIDisNot') = [])
+
+    def test_the_session_list_should_be_not_empty_if_the_userID_was_added(self):
+        sessionService.addSession('id','aSessionToken')
+        assertTrue(sessionService.getSessionsList('id') == ['aSessionToken'])
+
+    def test_the_session_list_should_be_acumulable(self):
+        sessionService.addSession('id','otherSessionToken')
+        assertTrue(sessionService.getSessionsList('id') == ['aSessionToken','aSessionToken'])
