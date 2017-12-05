82e0caf2e6d276c315071da6a375a9c4dc627268
cristian3629 <cristian3629@gmail.com>
2017-12-05 00:57:06 -0300

    src/llevame/shared_service/data_transformer_test.py

--- src/llevame/shared_service/data_transformer_test.py
+++ src/llevame/shared_service/data_transformer_test.py
@@ -4,16 +4,16 @@ from shared_service import dataTransformatorQueryParams as transformer
 
 class TestDataTransformer(unittest.TestCase):
     def test_the_result_should_be_two_when_type_is_driver(self):
-        self.assertTrue(transformer.transformateTypeUser('driver') == 2)
+        self.assertTrue(transformer.transformateTypeUser('driver') == '2')
 
 
     def test_the_result_should_be_one_when_type_is_passenger(self):
-        self.assertTrue(transformer.transformateTypeUser('passenger') == 1)
+        self.assertTrue(transformer.transformateTypeUser('passenger') == '1')
 
 
     def test_the_result_should_be_equals(self):
         data = {'name':'someName','last_name':'someLastName','email':'someEmail','type':'passenger'}
-        self.assertTrue(transformer.transformate(data) == 'name=someName&last_name=someLastName&email=someEmail&type=passenger')
+        self.assertTrue(transformer.transformate(data) == 'name=someName&last_name=someLastName&email=someEmail&type=passenger&')
 
     def test_the_result_shoud_be_equals_when_have_a_sub_object(self):
         data = {'name':'someName','last_name':'someLastName','email':'someEmail','card':{'number':'xx'}}
