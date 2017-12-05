9d52ad36126094ff18a199cbf8e426872a7f04e2
cristian3629 <cristian3629@gmail.com>
2017-12-04 23:50:22 -0300

    fix: arreglo un error de sintaxis en los test

--- src/llevame/session/session_service_test.py
+++ src/llevame/session/session_service_test.py
@@ -5,12 +5,12 @@ from session import session_service as sessionService
 
 class TestSessionService(unittest.TestCase):
     def test_the_session_list_should_be_empty_when_the_userID_is_not(self):
-        assertTrue(sessionService.getSessionsList('userIDisNot') == [])
+        self.assertTrue(sessionService.getSessionsList('userIDisNot') == [])
 
     def test_the_session_list_should_be_not_empty_if_the_userID_was_added(self):
         sessionService.addSession('id','aSessionToken')
-        assertTrue(sessionService.getSessionsList('id') == ['aSessionToken'])
+        self.assertTrue(sessionService.getSessionsList('id') == ['aSessionToken'])
 
     def test_the_session_list_should_be_acumulable(self):
         sessionService.addSession('id','otherSessionToken')
-        assertTrue(sessionService.getSessionsList('id') == ['aSessionToken','aSessionToken'])
+        self.assertTrue(sessionService.getSessionsList('id') == ['aSessionToken','aSessionToken'])
