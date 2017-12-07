dbc371b851455f7425aaf36d36c6fd13745221ba
Cristian <cristian3629@gmail.com>
2017-12-07 02:19:10 -0300

    agrego los self al servicio de sessiones

--- src/llevame/session/session_service.py
+++ src/llevame/session/session_service.py
@@ -14,12 +14,12 @@ class SessionService:
     def addSession(self,userID,sessionToken):
         logging.info('Guardando una session',extra=log_info)
         session_list = []
-        if userID in sessions.keys():
-            session_list = sessions[userID]
+        if userID in self.sessions.keys():
+            session_list = self.sessions[userID]
         session_list.append(sessionToken)
-        sessions[userID] = session_list
+        self.sessions[userID] = session_list
 
     def getSessionsList(self,userID):
-        if userID not in sessions.keys():
+        if userID not in self.sessions.keys():
             return []
-        return sessions[userID]
+        return self.sessions[userID]
