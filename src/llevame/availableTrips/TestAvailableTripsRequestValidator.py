085c8563de0d86458a8f98cdc47d9471f60e3ab9
Cristian <cristian3629@gmail.com>
2017-12-07 02:38:17 -0300

    agrego el prefijo test a los test del validador de request de viajes di

--- src/llevame/availableTrips/TestAvailableTripsRequestValidator.py
+++ src/llevame/availableTrips/TestAvailableTripsRequestValidator.py
@@ -6,11 +6,11 @@ from availaleTrips.availaleTripsRequestValidator import RequestValidator
 validator = RequestValidator()
 
 class TestUserService(unittest.TestCase):
-    def the_validate_should_return_false_because_is_not_from(self):
+    def test_the_validate_should_return_false_because_is_not_from(self):
         assertTrue(validator.validate({'no':'xx'}) != 'ok')
 
-    def the_validate_should_return_false_because_is_not_to(self):
+    def test_the_validate_should_return_false_because_is_not_to(self):
         assertTrue(validator.validate({'from':'xx'}) != 'ok')
 
-    def the_validate_should_return_true_because_is_complet(self):
+    def test_the_validate_should_return_true_because_is_complet(self):
         assertTrue(validator.validate({'from':'xx','to':'xx'}) == 'ok')
