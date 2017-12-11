b1abeaad6d9315bc4afa75b92f4ee47183c80119
Tomás Arjovsky <t.arjovsky@gmail.com>
2017-12-11 11:48:22 -0300

    data como parámetro

--- src/llevame/notification/notification_service.py
+++ src/llevame/notification/notification_service.py
@@ -12,23 +12,24 @@ log_info = {'clientip': '192.168.0.1', 'service': 'sharedService'}
 sessionService = SessionService()
 
 def notificate_user(userID,data):
-    logging.info('Se esta notificando al usuario:'+userID,extra=log_info)
+    logging.info('Se esta notificando al usuario: '+userID,extra=log_info)
+
     session_list = sessionService.getSessionsList(userID)
+    logging.info("Lista de sesiones para ese usuario: " + str(session_list), extra=log_info)
     for session in session_list:
-        notificate(session)
+        notificate(session, data)
     return True
 
 def notificate(sessionID,data):
 
     data_to_active_listener = {
-    'to':sessionID,
-    'android':{
-	   'ttl':'86400s',
-		    'notification' : {
-    		  'data':data
-		     }
-	 }
+        'to':sessionID,
+        'android':{
+    	   'ttl':'86400s',
+    		    'notification' : {
+        		  'data':data
+    		     }
+    	 }
     }
-
-    requests.post('https://fcm.googleapis.com/fcm/send',headers={'Content-Type':'application/json','Authorization':'key='+keyServer},data=data_to_active_listener)
+    pv = requests.post('https://fcm.googleapis.com/fcm/send',headers={'Content-Type':'application/json','Authorization':'key='+keyServer},data=data_to_active_listener)
     return True
